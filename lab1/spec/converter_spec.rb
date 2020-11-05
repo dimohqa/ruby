cf55d9d69368a8230016a610542f4a225c322bf5
dimohqa99 <3383015-dimohqa99@users.noreply.gitlab.com>
2020-11-05 22:21:36 +0700

    ammend commit

--- lab1/spec/converter_spec.rb
+++ lab1/spec/converter_spec.rb
@@ -1,28 +1,28 @@
 require './lib/converter'
 
 RSpec.describe Converter do
-  describe '.converter' do
-    context 'convert celsius to fahrenheit' do
+  describe 'converter' do
+    context 'celsius to fahrenheit' do
       it { expect(Converter.convert(10.0, 'C', 'F')).to eq 50.0 }
     end
 
-    context 'convert celsius to kelvin' do
+    context 'celsius to kelvin' do
       it { expect(Converter.convert(10.0, 'C', 'K')).to eq 283.15 }
     end
 
-    context 'convert fahrenheit to celsius' do
+    context 'fahrenheit to celsius' do
       it { expect(Converter.convert(10.0, 'F', 'C')).to eq(-12.22) }
     end
 
-    context 'convert fahrenheit to kelvin' do
+    context 'fahrenheit to kelvin' do
       it { expect(Converter.convert(10.0, 'F', 'K')).to eq 260.93 }
     end
 
-    context 'convert kelvin from celsius' do
+    context 'kelvin from celsius' do
       it { expect(Converter.convert(10.0, 'K', 'C')).to eq(-263.15) }
     end
 
-    context 'convert kelvin from fahrenheit' do
+    context 'kelvin from fahrenheit' do
       it { expect(Converter.convert(10.0, 'K', 'F')).to eq(-441.67) }
     end
   end
