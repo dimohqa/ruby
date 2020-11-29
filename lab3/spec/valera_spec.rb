f6d01a64fcc60c02a79d21bac0a1c3389dcea483
dimohqa99 <3383015-dimohqa99@users.noreply.gitlab.com>
2020-11-30 02:34:18 +0700

    refactoring tests

--- lab3/spec/valera_spec.rb
+++ lab3/spec/valera_spec.rb
@@ -1,39 +1,22 @@
 require './lib/valera'
 
 RSpec.describe Valera do
+  before(:each) do
+    state = {
+      'health' => 50,
+      'mana' => 40,
+      'fun' => 5,
+      'money' => 600,
+      'fatigue' => 5,
+      'death' => false
+    }
+    action_config = JSON.parse(File.read('./lib/config/action.json'))
+    boundaries = JSON.parse(File.read('./lib/config/boundaries.json'))
+    death_state = JSON.parse(File.read('./lib/config/death_state.json'))
+    @valera = Valera.new(state, action_config, boundaries, death_state)
+  end
+
   describe 'actions' do
-    before(:each) do
-      state = {
-        'health' => 50,
-        'mana' => 40,
-        'fun' => 5,
-        'money' => 600,
-        'fatigue' => 5,
-        'death' => false
-      }
-      action_config = JSON.parse(File.read('./lib/config/action.json'))
-      boundaries = JSON.parse(File.read('./lib/config/boundaries.json'))
-      death_state = JSON.parse(File.read('./lib/config/death_state.json'))
-      @valera = Valera.new(state, action_config, boundaries, death_state)
-    end
-
-    it 'change_attribute' do
-      @valera.change_attribute('mana', 50)
-      expect(@valera.state['mana']).to eq 90
-
-      @valera.change_attribute('health', 50)
-      expect(@valera.state['health']).to eq 100
-
-      @valera.change_attribute('fun', 50)
-      expect(@valera.state['fun']).to eq 10
-
-      @valera.change_attribute('money', 50)
-      expect(@valera.state['money']).to eq 650
-
-      @valera.change_attribute('fatigue', 50)
-      expect(@valera.state['fatigue']).to eq 55
-    end
-
     it 'work' do
       @valera.work
       expect(@valera.state['mana']).to eq 10
@@ -98,7 +81,9 @@ RSpec.describe Valera do
       expect(@valera.state['fatigue']).to eq 0
       expect(@valera.state['health']).to eq 50
     end
+  end
 
+  describe 'helpers' do
     it 'enough_money?' do
       expect(@valera.enough_money?(300)).to eq true
       expect(@valera.enough_money?(1000)).to eq false
@@ -117,9 +102,24 @@ RSpec.describe Valera do
       @valera.state['mana'] = 100
       expect(@valera.death?).to eq true
     end
+  end
 
-    it 'alcohol_action_change' do
-      @valera.alcohol_action_change('bar')
+  describe 'attribute' do
+    it 'change_attribute' do
+      @valera.change_attribute('mana', 50)
+      expect(@valera.state['mana']).to eq 90
+
+      @valera.change_attribute('health', 50)
+      expect(@valera.state['health']).to eq 100
+
+      @valera.change_attribute('fun', 50)
+      expect(@valera.state['fun']).to eq 10
+
+      @valera.change_attribute('money', 50)
+      expect(@valera.state['money']).to eq 650
+
+      @valera.change_attribute('fatigue', 50)
+      expect(@valera.state['fatigue']).to eq 55
     end
   end
 end
